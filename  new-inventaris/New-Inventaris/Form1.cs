7800b0d0c0d681a6040709ed29adf6bca5615d67
agussupriyantosaid@gmail.com <agussupriyantosaid@gmail.com@25b0de55-8a30-2434-f8a5-bcbd60f2cc37>
2012-07-25 08:12:18 +0000

    New inventaris + setup from server to connect mysql database

---
+++  new-inventaris/New-Inventaris/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace New_Inventaris
+{
+    public partial class FrmSever : Form
+    {
+        ToolTip tool = new ToolTip();
+        public FrmSever()
+        {
+            InitializeComponent();            
+            tool.UseFading = true;
+            tool.UseAnimation = true;
+
+            tool.ShowAlways = true;
+            tool.AutoPopDelay = 0;
+            tool.InitialDelay = 0;
+            tool.ReshowDelay = 0;
+            tool.SetToolTip(label1, "IP Server (ex = 192.168.1.100,localhost)");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
