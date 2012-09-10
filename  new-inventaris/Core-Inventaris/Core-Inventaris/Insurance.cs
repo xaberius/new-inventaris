32b9aeb100b0aa1eedc7d7ec7d07ca1ea953ad73
agussupriyantosaid@gmail.com <agussupriyantosaid@gmail.com@25b0de55-8a30-2434-f8a5-bcbd60f2cc37>
2012-09-10 09:02:11 +0000

    new inventaris form utama form Insurance new paradigm

---  new-inventaris/Core-Inventaris/Core-Inventaris/Insurance.cs
+++  new-inventaris/Core-Inventaris/Core-Inventaris/Insurance.cs
@@ -11,9 +11,9 @@ namespace Core_Inventaris
     public class Insurance
     {
         Connection Connetion = new Connection();
+        MySqlDataReader Reader;
         public List<InsuranceData> getData()
         {
-            MySqlDataReader Reader;
             Reader = Connetion.selectTable("select * from insurance");
            
             List<InsuranceData> InsDatas = new List<InsuranceData>();
@@ -28,19 +28,53 @@ namespace Core_Inventaris
                 InsData.Contact = Reader.GetString(5);
                 InsDatas.Add(InsData);
             }
+            Reader.Close();
             return InsDatas;
         }
 
-        public void toServer()
+        public bool toServer()
         {
             List<string> Database = Connetion.readSever();
 
-            if (!Connetion.connection(Database[0], Database[1], Database[2]))
+            try 
             {
-                MessageBox.Show("Database disconnect. Please contact your administrator.", "Database",
-                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (Connetion.connection(Database[0], Database[1], Database[2]))
+                {
+                    return true;
+                }
+                else 
+                {
+                    return false;
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return false;
             }
         }
-        
+
+        public string deleteData(string ID)
+        {
+            Reader = Connetion.selectTable("Delete from insurance where id='" + ID + "'");
+            Reader.Close();
+            return "Deleted!";
+        }
+
+        public string insertData(string ID,string InsuranceName,string Address, string City, string Phone, string Contact)
+        {
+            Reader = Connetion.selectTable("insert into insurance (id,insurancename,address,city,phone,contact) values ('" + ID + "','" +
+                InsuranceName + "','" + Address + "','" + City + "','" + Phone +"','" + Contact +"')");
+            Reader.Close();
+            return "Data Saved!";
+        }
+
+        public string updatetData(string ID, string InsuranceName, string Address, string City, string Phone, string Contact)
+        {
+            Reader = Connetion.selectTable("update insurance set insurancename = '"+ InsuranceName +"', address='"+ Address + "', city ='"+
+                    City + "',phone = '"+ Phone +"', contact = '"+ Contact +"' where id = '" + ID + "'");
+            Reader.Close();
+            return "Data Update!";
+        }
     }
 }
